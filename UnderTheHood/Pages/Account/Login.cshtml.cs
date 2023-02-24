1637df777820b7933709af21c0badaff12690c9c
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-02-24 12:58:31 +0300

    add login page

---
+++ UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                //Create security credintial
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", principal);
+
+                return RedirectToPage("/index");
+            }
+
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
--- UnderTheHood/Startup.cs
+++ UnderTheHood/Startup.cs
@@ -24,6 +24,7 @@ namespace UnderTheHood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddAuthentication().AddCookie("MyCookieAuth");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
